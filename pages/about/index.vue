d0e2bda6c122c92492bdbd3c15b369268cbe22ee
asim-ali-97 <asimali74372@gmail.com>
2023-09-22 03:50:59 +0500

    About page modified

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <h1>Welcome to about page</h1>
-        <p>this is my sub title</p>
+        <p>this is my sub title.. And it has been Modified..</p>
 
         <CustomBtn @click="$router.push('/')" title="Goto Home Page"/>
     </div>
